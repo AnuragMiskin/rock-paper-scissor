e079293462e15227dddfaa37fcc4cc3fb1b324ca
AnuragMiskin <anuragmiskin@gmail.com>
2023-08-20 19:25:40 +0530

    implemented condition for 5 wins

--- rps.js
+++ rps.js
@@ -5,6 +5,9 @@ function getcomputerchoice(){
     return choices[randomIndex];
 }
 function playround(mychoice){
+    if(win===5 || loss===5){
+        return;
+    }
     const computerchoice=getcomputerchoice();
     if(mychoice===computerchoice){
         document.querySelector('#choice').innerHTML="Player chooses:"+mychoice+"and computer chooses"+computerchoice+"<br> Its a draw";
@@ -16,78 +19,27 @@ function playround(mychoice){
     ){
         document.querySelector('#choice').innerHTML="Player chooses:"+mychoice+"and computer chooses"+computerchoice+"<br> you win";
         win++;
+        document.querySelector('#win').innerHTML="YOU:"+win;
         }
     else{
         document.querySelector('#choice').innerHTML="Player chooses:"+mychoice+"and computer chooses"+computerchoice+"<br> you lose";
         loss++;
+        document.querySelector('#loss').innerHTML="COMPUTER:"+loss;
     }
 }
 let win=0,loss=0;
-while(win<=5 || loss<=5){
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const mychoice = button.id;
         playround(mychoice);
+        if(win===5){
+            document.querySelector('#game').innerHTML="GAME OVER YOU WIN";
+            return;
+        }
+        else if(loss===5){
+            document.querySelector('#game').innerHTML="GAME OVER YOU LOSE";
+            return;
+        }
     });
 });
-}
-if(win==5){
-    //add div that displays win
-}
-else{
-    //add div that displays loss
-}
-
-/*
-const rock=document.querySelector('#rock');
-rock.addEventListener('click',()=>{
-    mychoice='rock';
-    playround(mychoice);
-});
-const paper=document.querySelector('#paper');
-paper.addEventListener('click',()=>{
-    mychoice='paper';
-    playround(mychoice);
-});
-const scissor=document.querySelector('#scissor');
-scissor.addEventListener('click',()=>{
-    mychoice='scissor';
-    playround(mychoice);
-});
-
-*/
-
-
-
-/*
-function playerchoice(){
-let choice=(prompt("enter your choice").toLowerCase());
-if((choice==='rock') || (choice==='paper') || (choice==='scissor')){
-return choice;
-}
-else{
-    console.error("choice not allowed");
-}
-}
-let win=0,loss=0;
-*/
-/*
-function playround(mychoice){
-    const computerchoice=getcomputerchoice();
-    if(mychoice===computerchoice){
-        document.queryselector('p').innerhtml ="player chooses:'+mychoice' and computer chooses '+computer choice+'<br> its a draw
-    }
-    else if(
-        (mychoice==='rock' && computerchoice==='scissor')|| 
-        (mychoice==='paper' && computerchoice==='rock')||
-        (mychoice==='scissor' && computerchoice==='paper')
-    ){
-           document.queryselector('p').innerhtml='
-           win++ 
-           document.queryselector(#you).innerhtml=+win- create a p tag in html to display playerchoice and computerchoice and a win and loss tags whose innerhtmls will be manipulated here
-        }
-    else{
-        -- same 
-        loss++;
-    }
